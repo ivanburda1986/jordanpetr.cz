b8accc5f1ad4a927d2d6b4fa002189952394bea3
ivanburda1986 <burda.ivan@gmail.com>
2020-06-01 17:30:28 +0200

    fixed the responsiveness

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,52 @@
+//Initialize the Gallery UI
+const gallery = new galleryUI();
+
+//GALLERY
+//UI Selectors
+const galleryOverview = document.getElementById("gallery-overview");
+const galleryHideButton = document.getElementById("gallery-hide-btn");
+const nextButton = document.getElementById("gallery-next-btn");
+const previousButton = document.getElementById("gallery-previous-btn");
+
+//Variables
+const ultimatePhotoNumber = photos.length;
+
+function getCurrentFullPhotoNumber() {
+  const currentFullPhoto = document.querySelector(".gallery-full-photo").id;
+  const currentFullPhotoNumber = photos.indexOf(currentFullPhoto) + 1;
+  return currentFullPhotoNumber;
+}
+
+//Populate the gallery overview
+gallery.listPhotos(photos);
+
+//Show the gallery overlay
+galleryOverview.addEventListener("click", (e) => {
+  gallery.showGalleryOverlay();
+  gallery.showFullPhoto(e.target.id);
+  gallery.clearPreviousFullPhotoDescription();
+  gallery.showCurrentFullPhotoDescription(getCurrentFullPhotoNumber, ultimatePhotoNumber);
+});
+
+//Show the next full photo
+nextButton.addEventListener("click", () => {
+  gallery.showNextFullPhoto(getCurrentFullPhotoNumber(), ultimatePhotoNumber);
+  gallery.clearPreviousFullPhotoDescription();
+  gallery.showCurrentFullPhotoDescription(getCurrentFullPhotoNumber, ultimatePhotoNumber);
+});
+
+//Show the previous full photo
+previousButton.addEventListener("click", () => {
+  gallery.showPreviousFullPhoto(getCurrentFullPhotoNumber(), ultimatePhotoNumber);
+  gallery.clearPreviousFullPhotoDescription();
+  gallery.showCurrentFullPhotoDescription(getCurrentFullPhotoNumber, ultimatePhotoNumber);
+});
+
+//Hide the gallery overlay
+galleryHideButton.addEventListener("click", (e) => {
+  gallery.hideGalleryOverlay();
+});
+
 //FOOTER
 //Get full year
 let fullYear = new Date().getFullYear();
---
+++ dist/js/gallery.js
@@ -0,0 +1,68 @@
+class galleryUI {
+  constructor() {
+    this.galleryOverview = document.getElementById("gallery-overview");
+    this.galleryOverlay = document.getElementById("gallery-overlay");
+    this.fullPhotoContainer = document.getElementById("gallery-full-photo-container");
+    this.currentFullPhotoDescription = document.getElementById("gallery-full-photo-description");
+  }
+
+  //Display in the UI an overview of available photos
+  listPhotos(photos) {
+    let galleryOverviewPhotos = "";
+    photos.forEach(photo => {
+      galleryOverviewPhotos += `
+      <div class="gallery-thumbnail" value="${photos.indexOf(photo)}">
+      <img src="img/gallery/thumbnails/${photo}" alt="${photo}" id="${photo}"
+      />
+      </div>
+      `
+    });
+    this.galleryOverview.innerHTML = galleryOverviewPhotos;
+  }
+
+  showGalleryOverlay() {
+    this.galleryOverlay.classList.add("show");
+    this.galleryOverlay.classList.remove("hide");
+  }
+
+  showFullPhoto(fullPhotoName) {
+    this.fullPhotoContainer.innerHTML = `<img src="img/gallery/${fullPhotoName}" alt="${fullPhotoName}" class="gallery-full-photo" id="${fullPhotoName}"/>`;
+  }
+
+  showNextFullPhoto(currentFullPhotoNumber, ultimatePhotoNumber) {
+    let nextPhotoNumber;
+    if (currentFullPhotoNumber === ultimatePhotoNumber) {
+      nextPhotoNumber = 1;
+    } else {
+      nextPhotoNumber = currentFullPhotoNumber + 1;
+    }
+
+    this.fullPhotoContainer.innerHTML = `<img src="img/gallery/photo${nextPhotoNumber}.jpg" alt="photo${nextPhotoNumber}.jpg" class="gallery-full-photo" id="photo${nextPhotoNumber}.jpg"/>`;
+    console.log(`This is photo number:${nextPhotoNumber}`);
+  }
+
+  showPreviousFullPhoto(currentFullPhotoNumber, ultimatePhotoNumber) {
+    let previousPhotoNumber;
+    if (currentFullPhotoNumber === 1) {
+      previousPhotoNumber = ultimatePhotoNumber;
+    } else {
+      previousPhotoNumber = currentFullPhotoNumber - 1;
+    }
+    console.log(`This is photo number:${previousPhotoNumber}`);
+    this.fullPhotoContainer.innerHTML = `<img src="img/gallery/photo${previousPhotoNumber}.jpg" alt="photo${previousPhotoNumber}.jpg" class="gallery-full-photo" id="photo${previousPhotoNumber}.jpg"/>`;
+
+  }
+
+  showCurrentFullPhotoDescription(getCurrentFullPhotoNumber, ultimatePhotoNumber) {
+    this.currentFullPhotoDescription.innerHTML = `<p>${getCurrentFullPhotoNumber()}/${ultimatePhotoNumber}: ${descriptions[getCurrentFullPhotoNumber()-1]}</p>`;
+  }
+
+  clearPreviousFullPhotoDescription() {
+    this.currentFullPhotoDescription.innerHTML = ``;
+  }
+
+  hideGalleryOverlay() {
+    this.galleryOverlay.classList.add("hide");
+  }
+
+}
