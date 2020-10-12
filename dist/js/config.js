77dddbd53746f68cd0221dae299505b949fc5886
ivanburda1986 <burda.ivan@gmail.com>
2020-10-13 00:11:09 +0200

    added photos and a note about pricing

--- dist/js/config.js
+++ dist/js/config.js
@@ -1,3 +1,3 @@
 //Gallery config
-const photos = ['photo1.jpg', 'photo2.jpg', 'photo3.jpg', 'photo4.jpg', 'photo5.jpg', 'photo6.jpg', 'photo7.jpg', 'photo8.jpg', 'photo9.jpg', 'photo10.jpg'];
-const descriptions = ['Srnci', 'Jelen', 'Rarity - srnci', 'Srnec', 'Srnec s lýčím', 'Muflon', 'Lebka', 'Jelen', 'Věšák', 'Věšák'];
+const photos = ['photo1.jpg', 'photo2.jpg', 'photo3.jpg', 'photo4.jpg', 'photo5.jpg', 'photo6.jpg', 'photo7.jpg', 'photo8.jpg', 'photo9.jpg', 'photo10.jpg','photo11.jpg','photo12.jpg','photo13.jpg'];
+const descriptions = ['Srnci', 'Jelen', 'Rarity - srnci', 'Srnec', 'Srnec s lýčím', 'Muflon', 'Lebka', 'Jelen', 'Věšák', 'Věšák', 'Hlavy před úpravou', 'Hlavy po úpravě', 'Raritní kus'];
