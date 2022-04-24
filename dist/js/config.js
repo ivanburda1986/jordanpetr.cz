d0e219f5549d72f13c48182c3d56e4a0ce3d68a4
Ivan Burda <ivan.burda@erstegroup.com>
2022-04-24 15:28:51 +0200

    New photos, update prices

--- dist/js/config.js
+++ dist/js/config.js
@@ -1,3 +1,3 @@
 //Gallery config
-const photos = ['photo1.jpg', 'photo2.jpg', 'photo3.jpg', 'photo4.jpg', 'photo5.jpg', 'photo6.jpg', 'photo7.jpg', 'photo8.jpg', 'photo9.jpg', 'photo10.jpg','photo11.jpg','photo12.jpg','photo13.jpg','photo14','photo15.jpeg','photo16.jpeg','photo17.jpeg'];
+const photos = ['photo1.jpg', 'photo2.jpg', 'photo3.jpg', 'photo4.jpg', 'photo5.jpg', 'photo6.jpg', 'photo7.jpg', 'photo8.jpg', 'photo9.jpg', 'photo10.jpg','photo11.jpg','photo12.jpg','photo13.jpg','photo14.jpg','photo15.jpg','photo16.jpg','photo17.jpg'];
 const descriptions = ['Srnci', 'Jelen', 'Rarity - srnci', 'Srnec', 'Srnec s lýčím', 'Muflon', 'Lebka', 'Jelen', 'Věšák', 'Věšák', 'Hlavy před úpravou', 'Hlavy po úpravě', 'Raritní kus', 'Jeleni parohy', 'Trofej','Hlavy před úpravou', 'Hlavy po úpravě'];
