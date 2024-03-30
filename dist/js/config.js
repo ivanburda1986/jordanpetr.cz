790f3248dbdf50ef06820970ecdfb02477f77ace
iBurda <burda.ivan@gmail.com>
2024-03-31 00:45:01 +0100

    popis

--- dist/js/config.js
+++ dist/js/config.js
@@ -1,3 +1,10 @@
 //Gallery config
-const photos = ['photo1.jpg', 'photo2.jpg', 'photo3.jpg', 'photo4.jpg', 'photo5.jpg', 'photo6.jpg', 'photo7.jpg', 'photo8.jpg', 'photo9.jpg', 'photo10.jpg','photo11.jpg','photo12.jpg','photo13.jpg','photo14.jpg','photo15.jpg','photo16.jpg','photo17.jpg'];
-const descriptions = ['Srnci', 'Jelen', 'Rarity - srnci', 'Srnec', 'Srnec s lýčím', 'Muflon', 'Lebka', 'Jelen', 'Věšák', 'Věšák', 'Hlavy před úpravou', 'Hlavy po úpravě', 'Raritní kus', 'Jeleni parohy', 'Trofej','Hlavy před úpravou', 'Hlavy po úpravě'];
+const photos = [
+    'photo1.jpg', 'photo2.jpg', 'photo3.jpg', 'photo4.jpg', 'photo5.jpg',
+    'photo6.jpg', 'photo7.jpg', 'photo8.jpg', 'photo9.jpg', 'photo10.jpg',
+    'photo11.jpg', 'photo12.jpg', 'photo13.jpg', 'photo14.jpg', 'photo15.jpg',
+    'photo16.jpg', 'photo17.jpg', 'photo18.jpeg', 'photo19.jpeg', 'photo20.jpeg',
+    'photo21.jpeg', 'photo22.jpeg', 'photo23.jpeg'
+];
+
+const descriptions = ['Srnci', 'Jelen', 'Rarity - srnci', 'Srnec', 'Srnec s lýčím', 'Muflon', 'Lebka', 'Jelen', 'Věšák', 'Věšák', 'Hlavy před úpravou', 'Hlavy po úpravě', 'Raritní kus', 'Jeleni parohy', 'Trofej','Hlavy před úpravou', 'Hlavy po úpravě', 'Ukázka práce','Ukázka práce','Ukázka práce','Ukázka práce','Ukázka práce','Ukázka práce'];
