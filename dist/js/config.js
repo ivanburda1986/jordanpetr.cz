1cd900b5549a03ac27b64ee4a7cb25922ef2a1eb
iBurda <burda.ivan@gmail.com>
2024-03-31 01:00:09 +0100

    extension

--- dist/js/config.js
+++ dist/js/config.js
@@ -4,7 +4,7 @@ const photos = [
     'photo6.jpg', 'photo7.jpg', 'photo8.jpg', 'photo9.jpg', 'photo10.jpg',
     'photo11.jpg', 'photo12.jpg', 'photo13.jpg', 'photo14.jpg', 'photo15.jpg',
     'photo16.jpg', 'photo17.jpg', 'photo18.jpg', 'photo19.jpg', 'photo20.jpg',
-    'photo21.jpg', 'photo22.jpg', 'photo23.jpeg'
+    'photo21.jpg', 'photo22.jpg', 'photo23.jpg'
 ];
 
 const descriptions = ['Srnci', 'Jelen', 'Rarity - srnci', 'Srnec', 'Srnec s lýčím', 'Muflon', 'Lebka', 'Jelen', 'Věšák', 'Věšák', 'Hlavy před úpravou', 'Hlavy po úpravě', 'Raritní kus', 'Jeleni parohy', 'Trofej','Hlavy před úpravou', 'Hlavy po úpravě', 'Trofej','Trofej','Trofej','Trofej','Trofej','Trofej'];
